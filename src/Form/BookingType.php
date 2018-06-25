2cff28c0759ac2940141d6ede1bfc4d1fabb348c
Marion <marion_ott@me.com>
2018-06-25 10:32:13 +0200

    issue #3 structure upload

---
+++ src/Form/BookingType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Booking;
+use App\Entity\Cabin;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookingType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('guest_quantity')
+            ->add('cabin')
+            ->add('month')
+            ->add('reference')
+	        ->getForm();
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Booking::class,
+        ]);
+    }
+}
