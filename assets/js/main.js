20192b46f651cec078f49167b3fa05781a4980cf
Anthony <anthony.reynaud@hetic.net>
2018-06-26 14:01:59 +0200

    header done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,23 @@
 const ui = {
   planets: document.querySelectorAll('.t'),
-  cards: document.querySelectorAll('.Card_ctnr'),
-  card: document.querySelectorAll('.Card')
+  cards: document.querySelectorAll('.Card_slider_ctnr'),
+  card: document.querySelectorAll('.Card'),
+  expNav: document.querySelector('#roll'),
+  menu: document.querySelector('.Header_nav_box_menu')
 }
 
 const style = {
   boxShad: "0 0 80px rgba(56, 30, 85, 1)"
 }
 
+// FLOAT ANIM
 TweenMax.to(ui.planets, 4, {
   y: 20,
   yoyo: true,
   repeat: -1
 });
 
-// Map
+// MAP
 for (let i = 0; i < ui.cards.length; i++) {
   ui.cards[i].addEventListener('click', () => {
     for (let a = 0; a < ui.planets.length; a++) {
@@ -29,7 +32,7 @@ for (let i = 0; i < ui.cards.length; i++) {
         ui.planets[a].style.boxShadow = "";
         ui.planets[a].style.width = "3em";
         ui.planets[a].style.height = "3em";
-        
+
       }
     }
   });
