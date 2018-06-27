707c1df3d17393d5c277253e0d56d92dc99e2ffd
Anthony <anthony.reynaud@hetic.net>
2018-06-27 17:37:21 +0200

    header done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,10 @@
 const ui = {
   cards: document.querySelectorAll('.Card_slider_ctnr'),
+  img: document.querySelector('.Map_img'),
+  burger: document.querySelector('.Header_burger'),
   menu: document.querySelector('.Header_nav_box_menu'),
-  img: document.querySelector('.Map_img')
+  nav: document.querySelector('.Header_nav'),
+  roll: document.querySelector('#roll')
 }
 
 const style = {
@@ -21,3 +24,13 @@ for (let i = 0; i < ui.cards.length; i++) {
     ui.img.setAttribute("src", "assets/img/p" + i + ".png");
   });
 }
+
+// HEADER
+ui.burger.addEventListener('click', () => {
+  ui.nav.classList.toggle('flex');
+});
+
+ui.roll.addEventListener('click', () => {
+    ui.menu.classList.toggle('flexCol');
+});
+
