2119d84266614e737a775d0ba878b03918d59fbd
Anthony <anthony.reynaud@hetic.net>
2018-06-28 17:36:07 +0200

    fuck the bitch

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,19 +1,24 @@
 const ui = {
   cards: document.querySelectorAll('.Card_slider_ctnr'),
-  img: document.querySelector('.Map_img')
+  img: document.querySelector('.Map_img'),
+  burger: document.querySelector('.Header_burger'),
+  menu: document.querySelector('.Header_nav_box_menu'),
+  nav: document.querySelector('.Header_nav'),
+  roll: document.querySelector('#roll')
 }
-
-// FLOAT ANIM
-TweenMax.to(ui.img, 4, {
-  y: 30,
-  yoyo: true,
-  repeat: -1
-});
 
 // MAP
 for (let i = 0; i < ui.cards.length; i++) {
   ui.cards[i].addEventListener('click', () => {
-    ui.img.setAttribute("src", "assets/img/p" + i + ".png");
+    ui.img.setAttribute("src", "/assets/img/p" + i + ".png");
   });
 }
 
+// HEADER
+ui.burger.addEventListener('click', () => {
+  ui.nav.classList.toggle('flex');
+});
+
+ui.roll.addEventListener('click', () => {
+  ui.menu.classList.toggle('flexCol');
+});
